580427ed5872b630d541d40c3a9ec344a92d45bb
vaishnavi349 <vankalasvaishnavi9@gmail.com>
2025-03-15 10:51:19 +0530

    Add files via upload

---
+++ second program.java
@@ -0,0 +1,12 @@
+public class firstprogram{
+
+
+     public static void main(String[]ar){
+         
+
+    Sytsteam.out.println("welcome to JAva World.");
+     Sytstem.out.println("This is My first program");
+
+  }
+
+}
