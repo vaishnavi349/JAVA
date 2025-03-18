c46abe7ecd68971cd3622c26a4d87d76a95a98d6
vaishnavi349 <vankalasvaishnavi9@gmail.com>
2025-03-18 08:13:52 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,15 @@
+package com.vaishnavi;
+
+public class Calculator {
+
+	
+		static int a=80;
+		static int b=40;
+		public static void main(String[] args) {
+			double result=0;
+			result= a/b;
+			System.out.println(result);
+
+	}
+
+}
