65a48936435a78d88bf146e2834f63e37d578d95
vaishnavi349 <vankalasvaishnavi9@gmail.com>
2025-03-21 20:44:12 +0530

    Add files via upload

---
+++ string.java
@@ -0,0 +1,16 @@
+package com.vaishnavi;
+
+public class string {
+	public static void main(string[] args) {
+		
+		string name ="Vaishnavi Vankalas";
+		system.out.println("name");
+		
+		int age=30;
+		System.out.println("age");
+		System.out.println("My name is Vaishnvi Vankalas");
+		
+		System.out.println(40);
+	}
+
+}
