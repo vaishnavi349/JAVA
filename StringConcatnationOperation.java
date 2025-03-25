f7a85a75841888dda27599896058237685ab187b
vaishnavi349 <vankalasvaishnavi9@gmail.com>
2025-03-25 08:26:14 +0530

    Add files via upload

---
+++ StringConcatnationOperation.java
@@ -0,0 +1,16 @@
+package com.vaishnavi;
+
+public class StringConcatnationOperation {
+	  
+	public static void main(String[] args) {
+		int a=10;
+		int b=34;
+		System.out.println(a+b);
+		String result="Addition of:";
+		
+		System.out.println(a+b+result);
+		System.out.println(result+a+b);
+		
+	}
+
+}
