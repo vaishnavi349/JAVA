a405786b36ef4dc3eaca4de3224b7d09d4523875
vaishnavi349 <vankalasvaishnavi9@gmail.com>
2025-04-08 07:47:50 +0530

    Add files via upload

---
+++ addition.java
@@ -0,0 +1,15 @@
+package com.vaishnavi;
+
+public class addition {
+
+	public static void main(String[] args) {
+		addition operation= new addition();
+		operation.add(10,20);
+		
+	}
+	public void add(int a, int b) {
+		int result=a+b;
+		System.out.println("Addition is: "+result);
+	}
+
+}
